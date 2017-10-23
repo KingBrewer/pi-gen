e5c0cb99bf86f1e6effa5594fbb5d71015153668
KingBrewer <king@browarnicki.pl>
2017-10-23 11:47:08 +0100

    moved gstreamer and alljoyn to lite

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -38,3 +38,7 @@ usermod --pass='*' root
 EOF
 
 rm -f ${ROOTFS_DIR}/etc/ssh/ssh_host_*_key*
+
+# opengl fixes
+ln -fs /opt/vc/lib/libbrcmGLESv2.so ${ROOTFS_DIR}/opt/vc/lib/libGLESv2.so
+ln -fs /opt/vc/lib/libbrcmEGL.so ${ROOTFS_DIR}/opt/vc/lib/libEGL.so
--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -6,6 +6,3 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
 
-#opengl fixes
-ln -fs /opt/vc/lib/libbrcmGLESv2.so ${ROOTFS_DIR}/opt/vc/lib/libGLESv2.so
-ln -fs /opt/vc/lib/libbrcmEGL.so ${ROOTFS_DIR}/opt/vc/lib/libEGL.so
