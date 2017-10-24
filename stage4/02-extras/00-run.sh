448804693f0495e74528d8879d58b752a0b11641
KingBrewer <king@browarnicki.pl>
2017-10-24 12:15:07 +0100

    changed hostname, changed permissions to control backlight

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -6,3 +6,5 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
 
+# allow user to control backlight
+echo 'SUBSYSTEM=="backlight",RUN+="/bin/chmod 666 /sys/class/backlight/%k/brightness /sys/class/backlight/%k/bl_power"' > "${ROOTFS_DIR}/etc/udev/rules.d/backlight-permissions.rules"
