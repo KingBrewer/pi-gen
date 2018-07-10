4a9e6c0fb58f96dff6a0ce1eec4d04fd12c3ea51
KingBrewer <king@browarnicki.pl>
2018-07-10 11:44:35 +0100

    pulseaudio and sound (hifiberry) configs

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -6,5 +6,9 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.local/share/desktop-directories"
 
+install -m 644 files/asound.conf "${ROOTFS_DIR}/etc"
+install -m 644 files/.asoundrc "${ROOTFS_DIR}/home/pi"
+
+
 # allow user to control backlight
 echo 'SUBSYSTEM=="backlight",RUN+="/bin/chmod 666 /sys/class/backlight/%k/brightness /sys/class/backlight/%k/bl_power"' > "${ROOTFS_DIR}/etc/udev/rules.d/backlight-permissions.rules"
