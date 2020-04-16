4cab2e9d55ad50d00a6e7a3d6a3afd38fe99c638
KingBrewer <king@browarnicki.pl>
2020-04-16 21:29:43 +0100

    libreoffice - otherwise UI doesnt work

--- stage4/02-extras/00-run.sh
+++ stage4/02-extras/00-run.sh
@@ -6,6 +6,10 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/shar
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/applications"
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/share/desktop-directories"
 
+# added option to disable screensaver
+install -m 644 files/lightdm.conf "${ROOTFS_DIR}/etc/lightdm/lightdm.conf"
+
+# custom sound settings
 install -m 644 files/asound.conf "${ROOTFS_DIR}/etc"
 install -m 644 files/.asoundrc "${ROOTFS_DIR}/home/pi"
 
